67dd947d30bacff4956300735a9461cf86ec5779
Siddhant Kumar <siddhantkumarupmanyu@gmail.com>
2021-09-29 13:15:49 +0530

    addNewTodo Test

--- src/main/ui/app_screen/AppViewModel.js
+++ src/main/ui/app_screen/AppViewModel.js
@@ -1,5 +1,26 @@
+// noinspection ES6UnusedImports
+// eslint-disable-next-line no-unused-vars
+import Database from "../../Database";
+// for making jsDoc recognize #database
+
+import TodoItem from "../../vo/TodoItem";
+
 export default class AppViewModel {
 
+    /**
+     * @type {Database}
+     */
+    #database
+
+    constructor(database) {
+        this.#database = database
+    }
 
 
+    /**
+     * @param {string} note
+     */
+    addNewTodo(note) {
+        this.#database.insertTodo(new TodoItem(note, false))
+    }
 }
--- src/test/ui/app_screen/AppViewModel.test.js
+++ src/test/ui/app_screen/AppViewModel.test.js
@@ -1,9 +1,75 @@
-import AppViewModel from "../../../main/ui/app_screen/AppViewModel";
+// noinspection JSUnresolvedFunction
 
-test("addNewItem", () => {
-    const appViewModel = new AppViewModel()
+import AppViewModel from "../../../main/ui/app_screen/AppViewModel";
+import Database from "../../../main/Database";
+import TodoItem from "../../../main/vo/TodoItem";
+
+// https://jestjs.io/docs/es6-class-mocks#automatic-mock
+jest.mock("../../../main/Database")
+
+beforeEach(() => {
+    // Clear all instances and calls to constructor and all methods:
+    Database.mockClear();
+})
+
+
+// I do not know, where should the responsibility of creating of TodoItem go?
+//  - in viewModel or in App.js or in AddItem.js
+//  - for now I am leaving it in viewModel
+
+test("addNewTodo", () => {
+    const database = new Database()
+    const appViewModel = new AppViewModel(database)
+
+    appViewModel.addNewTodo("note")
+
+    const insertTodo = Database.mock.instances[0].insertTodo
+
+    expect(insertTodo).toBeCalledTimes(1)
+    // expect(insertTodo).toHaveBeenCalledWith(new TodoItem("note2", false, TodoItem.NO_ID))
+    expect(insertTodo).toBeCalledWith(expect.todoItem(new TodoItem("note", false, TodoItem.NO_ID)))
+    // expect(insertTodo).toBeCalledWithItem()
+    // expect((insertTodo.mock.calls[0][0]).equals(new TodoItem("note2", false, TodoItem.NO_ID))).toBe(true)
 })
 
 test.skip("flipItemStatus", () => {
 
+})
+
+test.skip("jestMockSpike", () => {
+    // we are using automatic mock for mocking es6 class
+    // https://jestjs.io/docs/es6-class-mocks#automatic-mock
+
+    const database = new Database()
+    const item = new TodoItem("note", false, 0)
+    database.insertTodo(item)
+
+    // checking invoking of constructor
+    expect(Database).toBeCalledTimes(1)
+
+    // getting insertTodo mocked function
+    const insertTodo = Database.mock.instances[0].insertTodo
+
+    // checking invoking of function
+    expect(insertTodo).toBeCalledTimes(1)
+    expect(insertTodo).toBeCalledWith(item)
+})
+
+
+// IDK why the failure message is not getting printed
+expect.extend({
+    todoItem(received, expected) {
+        const isEqual = expected.equals(received)
+        if (isEqual) {
+            return {
+                message: () => `expected ${received} to not equal ${expected}`,
+                pass: true
+            }
+        } else {
+            return {
+                message: () => `expected ${received} to equal ${expected}`,
+                pass: false
+            }
+        }
+    }
 })
