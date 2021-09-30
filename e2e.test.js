c540ba17353079cb80afc7d1681d591ebc757ed2
Siddhant Kumar <siddhantkumarupmanyu@gmail.com>
2021-09-30 15:14:03 +0530

    add testcafe

---
+++ e2e.test.js
@@ -0,0 +1,17 @@
+import {Selector} from 'testcafe';
+
+// https://github.com/facebook/create-react-app/issues/6708#issuecomment-488390832
+
+// eslint-disable-next-line no-undef
+fixture`EndToEndTest`
+    .page`http://localhost:3000`
+
+test("addTodo", async testController => {
+    const textInput = Selector(".text-input")
+    const addButton = Selector(".button")
+        .withText("ADD")
+
+    await testController
+        .typeText(textInput, "newTodoItem")
+        .click(addButton)
+})
