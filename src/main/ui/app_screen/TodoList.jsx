b52ad77d4534fade475b1f8ec0739e5c9e15628d
Siddhant Kumar <siddhantkumarupmanyu@gmail.com>
2021-10-28 21:46:17 +0530

    intermediate commit
    i have two choices here;
    1. just go with ReactCSSTransitionGroup.
    2. do it yourself.
    applying cost vs benefit I think ReactCSSTransitionGroup will do a better job. Moreover, from reading docs it does not require list diff. It just needs a key in list item.
    But on the other side I like using list Diff and want to just try to make it work by myself.
    making branches here.
    first will do it with ReactCSSTransitionGroup. Then DIY.

--- src/main/ui/app_screen/TodoList.jsx
+++ src/main/ui/app_screen/TodoList.jsx
@@ -1,6 +1,6 @@
 import {Checkbox, List, MaterialIconButton} from "../base_components/BaseComponents";
 import './TodoList.scss';
-import {useRef} from "react";
+import {useEffect, useRef, useState} from "react";
 import {getListDiff} from "../ui_utils/ListDiff";
 
 export function TodoList({todoItems, onItemClick, onItemDelete}) {
@@ -13,14 +13,41 @@ export function TodoList({todoItems, onItemClick, onItemDelete}) {
 
     // we can optimize this further with https://reactjs.org/docs/hooks-faq.html#how-to-memoize-calculations
 
-    // should I wrap todoItems(in useRef and item.current) with Array.from()
-    // since react and my structure gives me a little bit of trust
-    // i am gonna ignore it for now
-    let items = useRef(todoItems)
+    // let items = useRef([])
 
-    const diffedList = getListDiff(items.current, todoItems, (item1, item2) => item1.equals(item2))
+    // todo: program is crashing when we flip the item status
+    //  - we can do two things here
+    //    - we can make equal here compare only ids; i.e. item1.getId() === item2.getId()
+    //    - second we can make sure we only diff when the length of oldItems is different than newList
+    //      - this way we make sure we never waste time diffing when there is no addition or removal
+    //      - for this case we do not care about if item is changed internally
+    //  - since I am not sure if react can postpone rendering; in case when react does it and
+    //    newList contains both changed items and added or removed items
+    //    we should therefore use both the above solutions
+    //  - one last thing; make test before writing any code; I know i am following TDD; but still
+    //    sometimes it's better to just remind oneself. :)
 
-    items.current = todoItems
+    // todo: lets get a visual spike first of animation
+    //  - this is visual thing; can only be tested manually
+
+    let [items, setItems] = useState([])
+
+    const diffedList = getListDiff(items, todoItems, (item1, item2) => item1.equals(item2))
+
+    // will run on all renders
+    // if want only on componentDidMount or once then pass [] i.e. empty array as dependency
+    // useEffect(() => {
+    //     items.current = todoItems
+    // })
+
+    useEffect(() => {
+        setTimeout(() => {
+            // todo: fix first item not showing animation
+            //  - write a test confirming the bug first
+            //  - remove Array.from line to reproduce the bug
+            setItems(Array.from(todoItems))
+        }, 50)
+    }, [todoItems])
 
     return (
         <div className="todo-list">
